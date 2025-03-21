5af28fbec31c5e4c55311fa6e3af47f5aa8fcce8
theo303 <theo.coutant32@gmail.com>
2025-03-21 15:58:35 +0100

    optimize

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/analysis/singlechecker"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 func main() {
@@ -12,62 +14,64 @@ func main() {
 }
 
 var Analyzer = &analysis.Analyzer{
-	Name: "gotransactornamedctx",
-	Doc:  "Checks that transactor.WithinTransaction has a named context parameter.",
-	Run:  run,
+	Name:     "gotransactornamedctx",
+	Doc:      "Checks that transactor.WithinTransaction has a named context parameter.",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	inspect := func(node ast.Node) bool {
-		callExpr, ok := node.(*ast.CallExpr)
-		if !ok {
-			return true
-		}
+	inspector := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{ // filter needed nodes: visit only them
+		(*ast.CallExpr)(nil),
+	}
+
+	inspector.Preorder(nodeFilter, func(node ast.Node) {
+		callExpr := node.(*ast.CallExpr)
 
 		selectorExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
 		if !ok {
-			return true
+			return
 		}
 
 		if selectorExpr.Sel == nil || selectorExpr.Sel.Name != "WithinTransaction" {
-			return true
+			return
 		}
 
 		selectorIdent, ok := selectorExpr.X.(*ast.Ident)
 		if !ok {
-			return true
+			return
 		}
 
 		// TODO: check if this change when renaming the import
 		if selectorIdent.Name != "transactor" {
-			return true
+			return
 		}
 
 		if len(callExpr.Args) != 2 {
-			return true
+			return
 		}
 
 		funcLit, ok := callExpr.Args[1].(*ast.FuncLit)
 		if !ok {
-			return true
+			return
 		}
 
 		if funcLit.Type == nil || funcLit.Type.Params == nil || len(funcLit.Type.Params.List) == 0 {
-			return true
+			return
 		}
 
 		field := funcLit.Type.Params.List[0]
 		if field == nil {
-			return true
+			return
 		}
 
 		if len(field.Names) == 0 {
 			pass.Reportf(node.Pos(), "transactor function has unnamed context parameter\n")
 		}
-		return true
-	}
-	for _, f := range pass.Files {
-		ast.Inspect(f, inspect)
-	}
+		return
+	})
+
 	return nil, nil
 }
